b60562a2827fe550e32b24604439878327e42939
yimd85 <yimd85@gmail.com>
2024-02-13 19:56:02 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+const App = () => {
+  return(
+    <div className='App'>
+      <Header/>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
